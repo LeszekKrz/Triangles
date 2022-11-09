911e4fc496b0ae144bfaeda21849a040c8342f10
Krzysztof Leszek <01161389@pw.edu.pl>
2022-11-09 15:27:23 +0100

    Add project files.

---
+++ Vertex.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangles
+{
+    internal class Vertex
+    {
+        int x;
+        int y;
+        int z;
+
+        int nx;
+        int ny;
+        int nz;
+
+        public Vertex(int x, int y, int z, int nx, int ny, int nz)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.nx = nx;
+            this.ny = ny;
+            this.nz = nz;
+        }
+    }
+}
