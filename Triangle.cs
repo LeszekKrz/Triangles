3f342f520955069710ff0d418b24c645b1c4577f
Chris <skoponer@gmail.com>
2022-11-14 21:58:03 +0100

    Psucie

--- Triangle.cs
+++ Triangle.cs
@@ -14,6 +14,9 @@ namespace Triangles
         Vertex b;
         Vertex c;
 
+        Color[] colors;
+        Point[] points;
+
         SolidBrush brush;
 
         public Vertex A { get { return a; } }
@@ -62,7 +65,7 @@ namespace Triangles
         {
             List<Vertex> vertices = new List<Vertex>() { a, b, c };
             vertices.Sort();
-            int[] indices = vertices.Select(v => this[v]).ToArray() ;
+            int[] indices = vertices.Select(v => this[v]).ToArray();
 
             List<ActiveEdge> AET = new List<ActiveEdge>();
             int y = this[indices[0]].ToPoint(size).Y;
@@ -75,40 +78,30 @@ namespace Triangles
 
             curr = this[indices[k]].ToPoint(size);
 
-            Color colorA = CalculateColor(simulationParameters, this[0]);
-            Color colorB = CalculateColor(simulationParameters, this[1]);
-            Color colorC = CalculateColor(simulationParameters, this[2]);
-
+            colors = new Color[3] { CalculateColor(simulationParameters, this[0]), CalculateColor(simulationParameters, this[1]), CalculateColor(simulationParameters, this[2]) };
+            points = new Point[3] { a.ToPoint(size), b.ToPoint(size), c.ToPoint(size) };
 
             while (y != this[indices[2]].ToPoint(size).Y)
             {
                 while (y == curr.Y)
                 {
-                    Vertex d1 = this[indices[k]];
-                    int d2 = this[d1];
-                    Vertex d3 = this[d2 + 3 - 1];
-
-                    Point pa = a.ToPoint(size);
-                    Point pb = b.ToPoint(size);
-                    Point pc = c.ToPoint(size);
-
                     prev = this[indices[k] + 3 - 1].ToPoint(size);
                     if (prev.Y > curr.Y)
                     {
-                        AET.Add(new ActiveEdge(curr, prev, CalculateColor(simulationParameters, this[indices[k]]), CalculateColor(simulationParameters, this[indices[k] + 3 - 1])));
+                        AET.Add(new ActiveEdge(indices[k], indices[k] - 1, curr.X, Angle(curr, prev)));
                     }
                     else if (prev.Y < curr.Y)
                     {
-                        AET.Remove(new ActiveEdge(prev, curr, Color.White, Color.White));
+                        AET.Remove(new ActiveEdge(indices[k] - 1, indices[k]));
                     }
                     next = this[indices[k] + 1].ToPoint(size);
                     if (next.Y > curr.Y)
                     {
-                        AET.Add(new ActiveEdge(curr, next, CalculateColor(simulationParameters, this[indices[k]]), CalculateColor(simulationParameters, this[indices[k] + 1])));
+                        AET.Add(new ActiveEdge(indices[k], indices[k] + 1, curr.X, Angle(curr,next)));
                     }
                     else if (next.Y < curr.Y)
                     {
-                        AET.Remove(new ActiveEdge(next, curr, Color.White, Color.White));
+                        AET.Remove(new ActiveEdge(indices[k] + 1, indices[k]));
                     }
                     k++;
                     curr = this[indices[k]].ToPoint(size);
@@ -116,14 +109,14 @@ namespace Triangles
                 AET.Sort();
                 for (int i = 0; i < AET.Count; i += 2)
                 {
-                    brush.Color = InterpolateColorOnLine(AET[i].Cu, AET[i].Cv, AET[i].U, AET[i].V, new Point((int)AET[i].X, y));
+                    brush.Color = InterpolateColorOnLine(AET[i], new Point((int)AET[i].X, y));
                     g.FillRectangle(brush, (int)Math.Round(AET[i].X), y, 1, 1);
                     for (int j = (int)Math.Round(AET[i].X) + 1; j < (int)Math.Round(AET[i + 1].X); j++)
                     {
-                        brush.Color = InterpolateColor(colorA, colorB, colorC, new Point(j,y), size);
+                        brush.Color = InterpolateColor(colors[0], colors[1], colors[2], new Point(j,y), size);
                         g.FillRectangle(brush, j, y, 1, 1);
                     }
-                    brush.Color = InterpolateColorOnLine(AET[i + 1].Cu, AET[i + 1].Cv, AET[i + 1].U, AET[i + 1].V, new Point((int)AET[i + 1].X, y));
+                    brush.Color = InterpolateColorOnLine(AET[i + 1], new Point((int)AET[i + 1].X, y));
                     g.FillRectangle(brush, (int)Math.Round(AET[i + 1].X), y, 1, 1);
                     AET[i].Step();
                     AET[i + 1].Step();
@@ -137,8 +130,9 @@ namespace Triangles
                 NormalVector N = v.Normal.GetVersor();
                 NormalVector R = 2 * N.Product(L) * N - L;
 
-                double lL = (double)simulationParameters.LightColor.R / 255;
-                double lO = (double)simulationParameters.ObjectColor.R / 255;
+                double lL, lO;
+                lL = (double)simulationParameters.LightColor.R / 255;
+                lO = (double)simulationParameters.ObjectColor.R / 255;
                 double l = simulationParameters.Kd * lL * lO * N.Cosinus(L) + simulationParameters.Ks * lL * lO * Math.Pow(simulationParameters.V.Cosinus(R), simulationParameters.M);
                 byte r = (byte)(l * 255);
 
@@ -159,77 +153,53 @@ namespace Triangles
 
             Color InterpolateColor(Color cA, Color cB, Color cC, Point v, int size)
             {
-                Point pA = a.ToPoint(size);
-                Point pB = this.b.ToPoint(size);
-                Point pC = c.ToPoint(size);
-                double area = Area(pA, pB, pC);
-                double areaA = Area(v, pB, pC);
-                double areaB = Area(pA, v, pC);
-                double areaC = Area(pA, pB, v);
+                TriangleRatios ratios = new TriangleRatios(this.a.ToPoint(size), this.b.ToPoint(size), this.c.ToPoint(size), v);
 
-                byte bA, bB, bC;
-
-                bA = cA.R;
-                bB = cB.R;
-                bC = cC.R;
-                //byte r = (byte)((bA * areaA / area + bB * areaB / area + bC * areaC / area) * 255);
-                byte r = (byte)(((double)bA * areaA / area + (double)bB * areaB / area + (double)bC * areaC / area));
-
-                bA = cA.G;
-                bB = cB.G;
-                bC = cC.G;
-                //byte g = (byte)((bA * areaA / area + bB * areaB / area + bC * areaC / area) * 255);
-                byte g = (byte)(((double)bA * areaA / area + (double)bB * areaB / area + (double)bC * areaC / area));
-
-                bA = cA.B;
-                bB = cB.B;
-                bC = cC.B;
-                //byte b = (byte)((bA * areaA / area + bB * areaB / area + bC * areaC / area) * 255);
-                byte b = (byte)(((double)bA * areaA / area + (double)bB * areaB / area + (double)bC * areaC / area));
+                byte r = (byte)ratios.Interpolate(cA.R, cB.R, cC.R);
+                byte g = (byte)ratios.Interpolate(cA.G, cB.G, cC.G);
+                byte b = (byte)ratios.Interpolate(cA.B, cB.B, cC.B);
 
                 return Color.FromArgb(255, r, g, b);
 
             }
 
-            Color InterpolateColorOnLine(Color cA, Color cB, Point pA, Point pB, Point v)
+            Color InterpolateColorOnLine(ActiveEdge edge, Point v)
             {
-                byte bA, bB;
-                double lA = Distance(pA, v);
-                double lB = Distance(pB, v);
-                double l = Distance(pA, pB);
 
-                bA = cA.R;
-                bB = cB.R;
-                byte r = (byte)((double)bA * lB / l + (double)bB * lA / l);
+                Color cA = colors[edge.I];
+                Color cB = colors[edge.J];
 
-                bA = cA.G;
-                bB = cB.G;
-                byte g = (byte)((double)bA * lB / l + (double)bB * lA / l);
-
-
-                bA = cA.B;
-                bB = cB.B;
-                byte b = (byte)((double)bA * lB / l + (double)bB * lA / l);
+                EdgeRatios ratios = new EdgeRatios(points[edge.I], points[edge.J], v);
+                byte r = (byte)ratios.Interpolate(cA.R, cB.R);
+                byte g = (byte)ratios.Interpolate(cA.G, cB.G);
+                byte b = (byte)ratios.Interpolate(cA.B, cB.B);
 
                 return Color.FromArgb(255, r, g, b);
 
 
             }
 
-            double Area(Point a, Point b, Point c)
-            {
-                double lA = Distance(a, b);
-                double lB = Distance(b, c);
-                double lC = Distance(c, a);
-                double p = (lA + lB + lC) / 2;
+        }
+        static public double Area(Point a, Point b, Point c)
+        {
+            double lA = Distance(a, b);
+            double lB = Distance(b, c);
+            double lC = Distance(c, a);
+            double p = (lA + lB + lC) / 2;
 
-                return Math.Sqrt(p * (p - lA) * (p - lB) * (p - lC));
-            }
+            return Math.Sqrt(p * (p - lA) * (p - lB) * (p - lC));
+        }
 
-            double Distance(Point a, Point b)
-            {
-                return Math.Sqrt((a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y));
-            }
+        public static double Distance(Point a, Point b)
+        {
+            return Math.Sqrt((a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y));
+        }
+
+        static double Angle(Point u, Point v)
+        {
+            if (u.Y != v.Y) return (v.X - u.X) / (double)(v.Y - u.Y);
+            else return 0;
+
         }
     }
 
@@ -238,32 +208,24 @@ namespace Triangles
         double x;
         double m;
 
-        Point u;
-        Point v;
-
-        Color cu;
-        Color cv;
+        int i;
+        int j;
 
         public double X => x;
 
-        public Color Cu { get => cu; }
-        public Color Cv { get => cv; }
+        public int I => i;
+        public int J => j;
 
-        public Point U { get => u; }
-
-        public Point V { get => v; }
-
-        public ActiveEdge(Point u, Point v, Color cu, Color cv)
+        public ActiveEdge(int i, int j, int x, double m)
         {
-            this.u = u;
-            this.v = v;
+            this.i = i >= 0 ? i % 3 : (i + 3) % 3;
+            this.j = j >= 0 ? j % 3 : (j + 3) % 3;
 
-            x = u.X;
-            if (u.Y != v.Y) m = (v.X - u.X) / (double)(v.Y - u.Y);
-            else m = 0;
+            this.x = x;
+        }
 
-            this.cu = cu;
-            this.cv = cv;
+        public ActiveEdge(int i, int j): this(i,j,0,0)
+        {
         }
 
         public int CompareTo(ActiveEdge? other)
@@ -274,13 +236,49 @@ namespace Triangles
         public override bool Equals(object? obj)
         {
             return obj is ActiveEdge edge &&
-                   u == edge.u &&
-                   v == edge.v;
+                   i == edge.i && j == edge.j;
         }
 
         public void Step()
         {
             x += m;
+        }
+    }
+
+    public class TriangleRatios
+    {
+        double ratioA;
+        double ratioB;
+        double ratioC;
+
+        public TriangleRatios(Point a, Point b, Point c, Point x)
+        {
+            double area = Triangle.Area(a, b, c);
+            ratioA = Triangle.Area(x, b, c) / area;
+            ratioB = Triangle.Area(a, x, c) / area;
+            ratioC = Triangle.Area(a, b, x) / area;
+        }
+
+        public double Interpolate(double parA, double parB, double parC)
+        {
+            return parA * ratioA + parB * ratioB + parC * ratioC;
+        }
+    }
+
+    public class EdgeRatios
+    {
+        double ratioA;
+        double ratioB;
+        public EdgeRatios(Point a, Point b, Point x)
+        {
+            double length = Triangle.Distance(a, b);
+            ratioA = Triangle.Distance(x, b) / length;
+            ratioB = Triangle.Distance(a, x) / length;
+        }
+
+        public double Interpolate(double parA, double parB)
+        {
+            return parA * ratioA + parB * ratioB;
         }
     }
 
